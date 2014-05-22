3c20ae422a548e7a4e90898590409016d1382bad
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-05-22 12:42:28 +0200

    rudimentary solution (n//4 holes of size 1)

---
+++ sol.cc
@@ -0,0 +1,53 @@
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+
+class RectanglesAndHoles {
+public:
+  vector<int> place(vector<int> A, vector<int> B) {
+    vector<int> result;
+    for (int i = 0; i < A.size(); i++) {
+      int idx = i / 4;
+      int x = idx % 10 * 2010;
+      int y = idx / 10 * 2010;
+      if (i % 4 == 0) {
+        result.push_back(x);
+        result.push_back(y - B[i]);
+        result.push_back(0);
+      } else if (i % 4 == 1) {
+        result.push_back(x + 1);
+        result.push_back(y);
+        result.push_back(0);
+      } else if (i % 4 == 2) {
+        result.push_back(x + 1 - A[i]);
+        result.push_back(y + 1);
+        result.push_back(0);
+      } else {
+        result.push_back(x - A[i]);
+        result.push_back(y + 1 - B[i]);
+        result.push_back(0);
+      }
+    }
+    return result;
+  }
+};
+
+
+int main(int argc, char *argv[]) {
+  int n;
+  cin >> n;
+  vector<int> A(n), B(n);
+  for (int i = 0; i < n; i++)
+    cin >> A[i];
+  for (int i = 0; i < n; i++)
+    cin >> B[i];
+
+  auto result = RectanglesAndHoles().place(A, B);
+  for (int r : result)
+    cout << r << endl;
+  cout.flush();
+  return 0;
+}
