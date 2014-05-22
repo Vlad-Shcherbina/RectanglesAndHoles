655448f5abda880671a0f4138ad74eb2976f6b5d
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-05-23 00:27:59 +0200

    big circle; change repacking algorithm

--- sol.cc
+++ sol.cc
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <thread>
 #include <cmath>
+#include <cstddef>
 
 #include "shortest.h"
 
@@ -56,20 +57,148 @@ vector<BoxPlacement> repack_placements(vector<vector<BoxPlacement>> bpss) {
   int cols = int(sqrt(bpss.size()) + 0.9999);
   int idx = 0;
   vector<BoxPlacement> result;
+
+  int h = 0;
+  int qx = 0;
+  int qy = 0;
+
   for (auto bps : bpss) {
-    int x0 = idx % cols * (size + 10);
-    int y0 = idx / cols * (size + 10);
+    if (idx % cols == 0) {
+      qy += h;
+      qx = 0;
+      h = 0;
+    }
     auto bb = placements_bb(bps);
     for (auto bp : bps) {
-      bp.bottom_left.X += x0 - bb.first.X;
-      bp.bottom_left.Y += y0 - bb.first.Y;
-      bp.top_right.X += x0 - bb.first.X;
-      bp.top_right.Y += y0 - bb.first.Y;
+      bp.bottom_left.X += qx - bb.first.X;
+      bp.bottom_left.Y += qy - bb.first.Y;
+      bp.top_right.X += qx - bb.first.X;
+      bp.top_right.Y += qy - bb.first.Y;
       result.push_back(bp);
     }
+    qx += bb.second.X - bb.first.X;
+    h = max(h, bb.second.Y - bb.first.Y);
     idx++;
   }
   return result;
+}
+
+
+class Arc {
+public:
+  vector<Box> boxes;
+  int delta_x, delta_y;
+  int64_t full_area;
+
+  Arc(vector<Box> boxes) : boxes(boxes) {
+    // TODO: randomize a and b
+    precompute();
+  }
+
+  void precompute() {
+    for (auto &box : boxes)
+      if (rand() % 2)
+        swap(box.a, box.b);
+    sort(boxes.begin(), boxes.end(),
+        [](Box b1, Box b2){ return b1.b * b2.a > b1.a * b2.b; });
+    delta_x = 0;
+    delta_y = 0;
+    full_area = 0;
+    for (auto box : boxes) {
+      full_area += int64_t(box.a) * int64_t(delta_y);
+      delta_x += box.a;
+      delta_y += box.b;
+    }
+  }
+
+  vector<BoxPlacement> place(int x_penalty = 0) {
+    vector<BoxPlacement> result;
+    int x = 0;
+    int y = 0;
+    //for (auto box : boxes) {
+    for (int i = 0; i < boxes.size(); i++) {
+      Box box = boxes[i];
+      int x_offset = 0;
+      if (i > 0 && x_penalty > 0) {
+        Box prev_box = boxes[i - 1];
+        x_offset = min(x_penalty, box.a + prev_box.a);
+        x_penalty -= x_offset;
+      }
+      //full_area += box.a * delta_y;
+      result.push_back(BoxPlacement::from_box(box, Coord(x - x_offset, y)));
+      x += box.a - x_offset;
+      y += box.b;
+    }
+    return result;
+  }
+};
+
+
+void transform(
+  vector<BoxPlacement> &bps,
+  int dx, int dy, bool mirror_x, bool mirror_y, bool mirror_xy) {
+  for (auto &bp : bps) {
+    for (auto p : {&bp.bottom_left, &bp.top_right}) {
+      if (mirror_x)
+        p->X *= -1;
+      if (mirror_y)
+        p->Y *= -1;
+      if (mirror_xy)
+        swap(p->X, p->Y);
+      p->X += dx;
+      p->Y += dy;
+    }
+    Coord bottom_left(
+        min(bp.bottom_left.X, bp.top_right.X),
+        min(bp.bottom_left.Y, bp.top_right.Y));
+    Coord top_right(
+        max(bp.bottom_left.X, bp.top_right.X),
+        max(bp.bottom_left.Y, bp.top_right.Y));
+    bp.bottom_left = bottom_left;
+    bp.top_right = top_right;
+  }
+}
+
+
+vector<BoxPlacement> make_circle(vector<Box> boxes) {
+  while (true) {
+    vector<Box> arc_boxes[4];
+    for (auto box : boxes)
+      arc_boxes[rand() % 4].push_back(box);
+    vector<BoxPlacement> result;
+    Arc arc[] = {arc_boxes[0], arc_boxes[1], arc_boxes[2], arc_boxes[3]};
+
+    int px = arc[0].delta_x + arc[1].delta_y - arc[2].delta_x - arc[3].delta_y;
+    int py = -arc[0].delta_y + arc[1].delta_x + arc[2].delta_y - arc[3].delta_x;
+    if (px < 0 || py < 0)
+      continue;
+
+    vector<BoxPlacement> t = arc[0].place(px);
+    arc[0].delta_x -= px;
+    copy(t.begin(), t.end(), back_inserter(result));
+
+    t = arc[1].place(py);
+    arc[1].delta_x -= py;
+    transform(t,
+        arc[0].delta_x, arc[0].delta_y,
+        true, false, true);
+    copy(t.begin(), t.end(), back_inserter(result));
+
+    t = arc[2].place();
+    transform(t,
+        arc[0].delta_x + arc[1].delta_y, arc[0].delta_y - arc[1].delta_x,
+        true, true, false);
+    copy(t.begin(), t.end(), back_inserter(result));
+
+    t = arc[3].place();
+    transform(t,
+        arc[0].delta_x + arc[1].delta_y - arc[2].delta_x,
+        arc[0].delta_y - arc[1].delta_x - arc[2].delta_y,
+        false, true, true);
+    copy(t.begin(), t.end(), back_inserter(result));
+
+    return result;
+  }
 }
 
 
@@ -77,6 +206,7 @@ class RectanglesAndHoles {
 public:
   vector<Box> boxes;
   vector<int> place(vector<int> A, vector<int> B) {
+    cerr << "n = " << A.size() << endl;
     boxes.clear();
     for (int i = 0; i < A.size(); i++) {
       Box box;
@@ -97,7 +227,12 @@ public:
 
     vector<vector<BoxPlacement> > solution;
 
-    cerr << "n = " << sorted_boxes.size() << endl;
+    int k = A.size() / 2;
+    vector<Box> for_circle(sorted_boxes.begin(), sorted_boxes.begin() + k);
+    sorted_boxes.erase(sorted_boxes.begin(), sorted_boxes.begin() + k);
+
+    solution.push_back(make_circle(for_circle));
+
     vector<int> result;
     for (int i = 0; i + 4 <= sorted_boxes.size(); i += 4) {
       int x = 0;
